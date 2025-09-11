728a9e5aab375d4b30c10ac124c194e71516470a
Kai Wei Mo <kaiweimo5@gmail.com>
2025-09-11 17:10:02 -0500

    add bash aliases for cd, clear, watch, history

--- aliases/aliases_files/generic.sh
+++ aliases/aliases_files/generic.sh
@@ -11,4 +11,25 @@ alias '....'='cd ../../..'
 alias '.....'='cd ../../../..'
 alias '......'='cd ../../../../..'
 
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
+alias ..5='cd ../../../../..'
+alias ..6='cd ../../../../../..'
+alias ..7='cd ../../../../../../..'
+alias ..8='cd ../../../../../../../..'
+alias ..9='cd ../../../../../../../../..'
+alias ..10='cd ../../../../../../../../../..'
+
 alias watch='watch '
+alias watchfast='watch -n 0.2'
+
+# clear
+alias celar='clear'
+alias cleare='clear'
+alias cealr='clear'
+alias c='clear'
+
+# history
+alias h='history'
+alias hist='history'
